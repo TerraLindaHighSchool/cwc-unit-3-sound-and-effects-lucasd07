6638bf9fb50601554e89e9eecf375225e4ef0bcb
Lucas Diamantidis <47400463+lykos7@users.noreply.github.com>
2020-02-06 14:37:39 -0800

    Tutorial 3.2.5 Completed

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private float gravityModifier;
     [SerializeField] private bool isOnGround = true;
+    [SerializeField] private bool gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +29,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
